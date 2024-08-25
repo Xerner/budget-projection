1b18417fa12ea4892b47b75a41f67a7d86e4a47c
Kenneth Mead <48263885+Xerner@users.noreply.github.com>
2024-08-25 13:41:00 -0400

    Push to cloud

--- src/stores/query-params.store.ts
+++ src/stores/query-params.store.ts
@@ -1,4 +1,4 @@
-import { effect, Injectable, signal, WritableSignal } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 
@@ -11,19 +11,11 @@ export enum QueryParamKey {
 @Injectable({
   providedIn: 'root'
 })
-export class QueryParamsService {
+export class QueryParamsStore {
   queryParams: Record<QueryParamKey, BehaviorSubject<any>> = {
     [QueryParamKey.baseName]: new BehaviorSubject<string | null>(null),
     [QueryParamKey.tableName]: new BehaviorSubject<string | null>(null),
     [QueryParamKey.token]: new BehaviorSubject<string | null>(null),
-  }
-
-  subscribe(name: QueryParamKey) {
-    return this.queryParams[name].asObservable();
-  }
-
-  set(name: QueryParamKey, value: any) {
-    this.queryParams[name](value);
   }
 
   constructor(
@@ -32,14 +24,24 @@ export class QueryParamsService {
   ) {
     for (const key in QueryParamKey) {
       if (this.queryParams[key as keyof typeof QueryParamKey]) {
-        this.addUpdateQueryParamInUrlEffect(key as QueryParamKey);
+        this.subscribeToKeepQueryParamsInUrlUpToDate(key as QueryParamKey);
       }
     }
   }
 
-  private addUpdateQueryParamInUrlEffect(queryParamKey: QueryParamKey) {
-    effect(() => {
-      var value = this.queryParams[queryParamKey]();
+  private subscribeToKeepQueryParamsInUrlUpToDate(queryParamKey: QueryParamKey) {
+    this.route.queryParamMap.subscribe((params) => {
+      const value = params.get(queryParamKey);
+      if (value == this.queryParams[queryParamKey].value) {
+        return;
+      }
+      this.queryParams[queryParamKey].next(value);
+    });
+    this.queryParams[queryParamKey].subscribe((value) => {
+      const urlValue = this.route.snapshot.queryParamMap.get(queryParamKey);
+      if (urlValue == value) {
+        return;
+      }
       this.updateQueryParam(queryParamKey, value);
     });
   }
