1410ee1aeab6fb41f29f29251d7c7a974254a302
Kenneth Mead <48263885+Xerner@users.noreply.github.com>
2024-08-30 15:56:30 -0400

    Fixed dev container

--- .devcontainer/setup-ssh-env.ps1
+++ .devcontainer/setup-ssh-env.ps1
@@ -11,6 +11,8 @@ $GIT_EMAIL = git config --global user.email
 
 # update ssh key filename as necessary
 $SSH_PUBLIC=$(Get-Content ~/.ssh/$SSH_KEYNAME.pub)
+# replace all new line characters with '\n'
+# see .bashrc for how its converted back to new lines when inside the container
 $SSH_PRIVATE=$(Get-Content ~/.ssh/$SSH_KEYNAME) -join "\n"
 $envString = "GIT_USER=$GIT_USER`nGIT_EMAIL=$GIT_EMAIL`nSSH_KEYNAME=$SSH_KEYNAME`nSSH_HOSTNAME=$SSH_HOSTNAME`nSSH_PUBLIC=$SSH_PUBLIC`nSSH_PRIVATE=$SSH_PRIVATE"
 $envPath = ".devcontainer/.env"
